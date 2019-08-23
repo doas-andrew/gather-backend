808b7e3a4a304fa79c7e475e3ffa9b1c21b20bf7
Andrew Allen <asallen67@gmail.com>
2019-08-23 15:56:29 -0400

    heroku env_vars

--- app/models/ssl.rb
+++ app/models/ssl.rb
@@ -2,8 +2,8 @@ class SSL
 	# @@public_key = OpenSSL::PKey::RSA.new(File.read(File.expand_path('../keys/ssl_keys/public.pem', __dir__)))
 	# @@private_key = OpenSSL::PKey::RSA.new(File.read(File.expand_path('../keys/ssl_keys/private.pem', __dir__)))
 
-	@@public_key = SSL_PUBLIC_KEY
-	@@private_key = SSL_PRIVATE_KEY
+	@@public_key = ENV["SSL_PUBLIC_KEY"]
+	@@private_key = ENV["SSL_PRIVATE_KEY"]
 	
 	def self.encrypt(plaintext)
 		Base64.encode64( @@public_key.public_encrypt(plaintext) )
