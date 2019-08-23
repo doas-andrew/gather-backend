f3b27523b421daa244b5e44bc72838dbe195241e
Andrew Allen <asallen67@gmail.com>
2019-08-23 15:47:01 -0400

    ssl keys set to env vars

--- app/models/ssl.rb
+++ app/models/ssl.rb
@@ -1,6 +1,9 @@
 class SSL
-	@@public_key = OpenSSL::PKey::RSA.new(File.read(File.expand_path('../keys/ssl_keys/public.pem', __dir__)))
-	@@private_key = OpenSSL::PKey::RSA.new(File.read(File.expand_path('../keys/ssl_keys/private.pem', __dir__)))
+	# @@public_key = OpenSSL::PKey::RSA.new(File.read(File.expand_path('../keys/ssl_keys/public.pem', __dir__)))
+	# @@private_key = OpenSSL::PKey::RSA.new(File.read(File.expand_path('../keys/ssl_keys/private.pem', __dir__)))
+
+	@@public_key = SSL_PUBLIC_KEY
+	@@private_key = SSL_PRIVATE_KEY
 	
 	def self.encrypt(plaintext)
 		Base64.encode64( @@public_key.public_encrypt(plaintext) )
